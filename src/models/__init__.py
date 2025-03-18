79232d02501a09c112e41b504194e491ac4d9527
JoseJuan98 <josejuan-98@hotmail.com>
2025-03-18 23:49:32 +0100

    time series modeling

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """Import shortcuts for models."""
 
+# First party imports
+from models.time_series.transformer import EncoderOnlyTransformerTSClassifier
+
 # Local imports
 from .positional_encoding.sinusoidal_positional_encoding import SinusoidalPositionalEncoding
-from .transformer.transformer import EncoderOnlyTransformerTSClassifier
