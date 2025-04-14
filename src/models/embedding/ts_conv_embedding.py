8a2151fa6ec3d792da16a7c19c8cb81d63b2e2e9
JoseJuan98 <josejuan-98@hotmail.com>
2025-04-15 00:26:46 +0200

    improved licensing

--- src/models/embedding/ts_conv_embedding.py
+++ src/models/embedding/ts_conv_embedding.py
@@ -1,5 +1,34 @@
 # -*- coding: utf-8 -*-
-"""Time Series Convolutional Embedding Layer."""
+"""Time Series Convolutional Embedding Layer.
+
+The TimeSeries1dConvEmbedding class in this module is adapted from the Time-Series-Library, licensed under the MIT License.
+
+Original code:
+https://github.com/thuml/Time-Series-Library/blob/40da06fef6e703b62c86bbea1c3d798f15a73fcb/layers/Embed.py#L29-L42
+
+
+Copyright (c) 2021 THUML @ Tsinghua University
+
+Licensed under the MIT License.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
 # Third party imports
 import torch
 
