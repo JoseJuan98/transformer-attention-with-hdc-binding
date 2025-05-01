63d12b47344ad0022bb4d49310013a422cb8eb10
JoseJuan98 <josejuan-98@hotmail.com>
2025-05-02 00:09:02 +0200

    choose running experiment

--- src/experiments/time_series/run.py
+++ src/experiments/time_series/run.py
@@ -7,10 +7,30 @@ import pathlib
 from experiment_framework import ExperimentConfigFactory, ExperimentRunner, ModelConfigFactory
 
 
+def choose_experiment_to_run() -> str:
+    """Prompt the user to choose an experiment to run."""
+    print("Choose an experiment to run:")
+    print("1. TS Classification with different binding methods")
+    print("2. TS Classification with different positional encodings")
+
+    experiments = {
+        1: "binding_methods",
+        2: "positional_encodings",
+    }
+
+    choice_msg = "> Enter the number of the experiment you want to run: "
+    choice = int(input(choice_msg))
+
+    while choice not in experiments.keys():
+        choice = int(input(f"> Invalid choice. Please enter {list(experiments.keys())}: "))
+
+    return experiments[choice]
+
+
 def run_time_series_experiments() -> None:
     """Run time series experiments for the Transformer model with different positional encodings."""
 
-    current_dir = pathlib.Path(__file__).resolve().parent / "binding_methods"
+    current_dir = pathlib.Path(__file__).resolve().parent / choose_experiment_to_run()
 
     model_configs = ModelConfigFactory().create_model_configs(model_cfg_path=current_dir / "models_cfg.json")
 
