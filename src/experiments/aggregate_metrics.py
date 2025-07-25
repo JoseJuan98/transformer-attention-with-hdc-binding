1ed67ce25105cf4fdee4916ae60e7045413a6aa9
JoseJuan98 <josejuan-98@hotmail.com>
2025-07-25 17:59:14 +0200

    added baseline metrics to SOTA experiment

--- src/experiments/aggregate_metrics.py
+++ src/experiments/aggregate_metrics.py
@@ -77,8 +77,7 @@ if __name__ == "__main__":
         # / "docs/experiment_results/1_binding_version_1/metrics_binding_version_1.csv"
         # / "docs/experiment_results/2_binding_N_L_4/metrics_binding_N_l_4.csv"
         # / "docs/experiment_results/3_pe_version_1/metrics_pe_version_1.csv"
-        # / "docs/experiment_results/4_sota_version_1/metrics_sota_version_1.csv"
-        / "docs/experiment_results/1_binding_version_2/metrics_binding_version_1.csv"
+        / "docs/experiment_results/4_sota_version_1/metrics_sota_version_1.csv"
     )
 
     # Define output directory
