3828db5dc3d4a85f1d06384ece77e15d52beb48f
JoseJuan98 <josejuan-98@hotmail.com>
2025-08-10 00:18:57 +0200

    added exp 2 $N_L$ plots

--- src/experiments/pe_visualization/n_l_experiment.py
+++ src/experiments/pe_visualization/n_l_experiment.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Visualizations of Experiment 3 for different size $d_{model$} binding operations."""
+"""Visualizations of Experiment 2 for different size $N_L$ binding operations."""
 # Standard imports
 import pathlib
 
@@ -27,13 +27,13 @@ def plot_metrics_by_binding(metrics: pandas.DataFrame, plot_path: pathlib.Path) 
     fig, ax = pyplot.subplots(figsize=(19, 10))
 
     models = ["additive", "component_wise", "circular_conv"]
-    d_models = [32, 64, 128, 256, 512, 1024]
-    x_ticks = list(range(len(d_models)))
-    lower_limit = 0.52
+    n_ls = [1, 2, 4, 8]
+    x_ticks = list(range(len(n_ls)))
+    lower_limit = 0.50
 
     for model in models:
         # Original accuracy data
-        acc = [metrics[metrics["model"] == f"linear_{model}_{d_model}"]["mean_acc"].values[0] for d_model in d_models]
+        acc = [metrics[metrics["model"] == f"linear_{model}_N_L_{n_l}"]["mean_acc"].values[0] for n_l in n_ls]
 
         # Create a set of smooth x-coordinates for the spline
         x_smooth = numpy.linspace(min(x_ticks), max(x_ticks), 300)
@@ -53,23 +53,14 @@ def plot_metrics_by_binding(metrics: pandas.DataFrame, plot_path: pathlib.Path) 
 
         # Annotation loop remains the same, as it annotates the original points
         for i, v in enumerate(acc):
-            if v < lower_limit:
-                ax.annotate(
-                    f"{v * 100:.2f}",
-                    xy=(i - 1, 0.5225),
-                    xytext=(9, 1),
-                    textcoords="offset points",
-                    annotation_clip=False,
-                )
-            else:
-                ax.annotate(f"{v * 100:.2f}", xy=(i, v), xytext=(-7, 7), textcoords="offset points")
+            ax.annotate(f"{v * 100:.2f}", xy=(i, v), xytext=(-7, 7), textcoords="offset points")
 
     ax.set_xticks(x_ticks)
-    ax.set_xticklabels(d_models, rotation=45)
+    ax.set_xticklabels(n_ls)
     ax.set_ylim([lower_limit, 0.6525])
     ax.set_xlabel("d_model")
     ax.set_ylabel("Mean Accuracy")
-    ax.set_title("Mean Accuracy by $d_{model}$ for Different Binding Operations")
+    ax.set_title("Mean Accuracy by $N_L$ for Different Binding Operations")
 
     # Add a legend
     ax.legend(loc="lower left")
@@ -91,12 +82,13 @@ if __name__ == "__main__":
 
     # Path to experiment's raw metrics CSV file
     experiment_metrics_path = (
-        pathlib.Path(__file__).parents[3] / "docs/experiment_results/3_d_model_v1/summary_model_metrics_d_model_v1.csv"
+        pathlib.Path(__file__).parents[3]
+        / "docs/experiment_results/2_N_L_version_1/summary_model_metrics_N_L_version_1.csv"
     )
     metrics_by_model = pandas.read_csv(filepath_or_buffer=experiment_metrics_path, header=0)
 
     # Plot path
-    plot_path = Config.plot_dir / "experiment" / "3_d_model_v1" / "d_model_v1_accuracy_plot.png"
+    plot_path = Config.plot_dir / "experiment" / "2_N_L_version_1" / "n_l_v1_accuracy_plot.png"
 
     # Create the plot directory if it doesn't exist
     plot_path.parent.mkdir(parents=True, exist_ok=True)
